b2accf8290db53a7a9b3da33f2e9ac8f92aeab6a
João Seixas <jnuno1997@gmail.com>
2020-07-24 15:42:18 +0100

    trying to fix values of string with multiple extra spaces

--- scraper.py
+++ scraper.py
@@ -6,14 +6,13 @@ from bs4 import BeautifulSoup
 nos_url = 'https://cinemas.nos.pt'
 response = requests.get(nos_url)
 
-print(response)
+#print(response)
+# Returns: <Response [200]>
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
 dropdown_movies = soup.find('article', class_='button is-hidden')
-
 movies_a = dropdown_movies.find_all('a', class_='list-item')
-
 
 movies = []
 mid = 0
@@ -28,3 +27,24 @@ for item in movies_a:
 
 print(movies)
 
+for m in movies:
+    mresponse = requests.get(m['url'])
+    msoup = BeautifulSoup(mresponse.content, 'html.parser')
+
+    description = msoup.find('section', class_='description').find_all('p')
+
+    for item in description:
+        name = item.b.text[:-1]
+        
+        if name == 'Data de estreia:':
+            value = ' '.join(item.span.text.split())
+        else:
+            item.b.extract()
+            value = ' '.join(item.text.split())
+
+        m[name] = ' '.join(value.split())
+    
+    sinopse = msoup.find('section', class_='sinopse').find('div', id='ctl00_PlaceHolderMain_tfSinopse__ControlWrapper_RichHtmlField').text
+    m['Sinopse'] = sinopse
+
+print(movies)
